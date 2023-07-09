b3aee2610b73b210099c02273ecb93af917a9ecf
nguyenhung98 <viethung.cse@gmail.com>
2023-07-09 11:40:43 +0700

    update exercise 2

--- exercises/ownership-borrowing/src/lib.rs
+++ exercises/ownership-borrowing/src/lib.rs
@@ -3,7 +3,7 @@
 fn exercise1() {
     // Use as many approaches as you can to make it work
     let x = String::from("hello, world");
-    let y = x;
+    let y = x.clone();
     let z = x;
 }
 
@@ -17,8 +17,9 @@ fn exercise2() {
     println!("{}", s2);
 }
 // Only modify the code below!
-fn take_ownership(s: String) {
+fn take_ownership(s: String) -> String {
     println!("{}", s);
+    s
 }
 
 // Exercise 3
@@ -41,8 +42,8 @@ fn exercise3() {
         let mut addition: f64 = 0.0;
 
         // Sumar valores en additions
-        for element_index in additions {
-            let addition_aux = values[element_index];
+        for element_index in &additions {
+            let addition_aux = values[*element_index];
             addition = addition_aux + addition;
         }
     }
@@ -50,10 +51,9 @@ fn exercise3() {
 
 // Exercise 4
 // Make it compile
-fn exercise4(value: u32) -> &'static str {
+fn exercise4(value: u32) -> String {
     let str_value = value.to_string(); // Convert u32 to String
-    let str_ref: &str = &str_value; // Obtain a reference to the String
-    str_ref // Return the reference to the String
+    str_value
 }
 
 // Exercise 5
@@ -67,9 +67,9 @@ fn exercise5() {
     let res = match my_map.get(&key) {
         Some(child) => child,
         None => {
-            let value = "3.0".to_string();
-            my_map.insert(key, value);
-            &value // HERE IT FAILS
+            let value = "3.0";
+            my_map.insert(key, value.to_string());
+            value // HERE IT FAILS
         }
     };
 
@@ -82,14 +82,14 @@ fn exercise5() {
 use std::io;
 
 fn exercise6() {
-    let mut prev_key: &str = "";
+    let mut prev_key = String::new();
 
     for line in io::stdin().lines() {
         let s = line.unwrap();
 
         let data: Vec<&str> = s.split("\t").collect();
         if prev_key.len() == 0 {
-            prev_key = data[0];
+            prev_key = data[0].to_string();
         }
     }
 }
@@ -97,11 +97,11 @@ fn exercise6() {
 // Exercise 7
 // Make it compile
 fn exercise7() {
-    let mut v: Vec<&str> = Vec::new();
+    let mut v: Vec<String> = Vec::new();
     {
-        let chars = [b'x', b'y', b'z'];
-        let s: &str = std::str::from_utf8(&chars).unwrap();
-        v.push(&s);
+        let chars: [u8; 3] = [b'x', b'y', b'z'];
+        let s: String = String::from_utf8(chars.to_vec()).unwrap();
+        v.push(s);
     }
     println!("{:?}", v);
 }
@@ -109,7 +109,7 @@ fn exercise7() {
 // Exercise 8
 // Make it compile
 fn exercise8() {
-    let mut accounting = vec!["Alice", "Ben"];
+    let mut accounting = vec!["Alice".to_owned(), "Ben".to_owned()];
     
     loop {
         let mut add_input = String::from("");
@@ -126,6 +126,6 @@ fn exercise8() {
         }
 
         let person = add_vec[0];
-        accounting.push(person);
+        accounting.push(person.to_owned());
     }
 }
