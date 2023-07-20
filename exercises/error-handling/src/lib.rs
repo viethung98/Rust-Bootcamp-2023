74cfca467b310013654e22538a0e623cc5749952
nguyenhung <hung.v.nguyen@s6klabs.com>
2023-07-20 18:41:18 +0700

    submit solution

--- exercises/error-handling/src/lib.rs
+++ exercises/error-handling/src/lib.rs
@@ -2,12 +2,12 @@
 // Make it compile in unit test
 // Run tests
 // Hint: Convert Option to Result
-fn generate_nametag_text(name: String) -> Option<String> {
+fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
         // Empty names aren't allowed.
-        None
+        Err("`name` was empty; it must be nonempty.".to_string())
     } else {
-        Some(format!("Hi! My name is {}", name))
+        Ok(format!("Hi! My name is {}", name))
     }
 }
 // Exercise 2
@@ -16,8 +16,11 @@ fn generate_nametag_text(name: String) -> Option<String> {
 // Hint: &str to integer conversion by using parse method and return Result
 use std::num::ParseIntError;
 
-fn parse_number(s: &str) -> Result<i32, ParseIntError> {
-    todo!()
+fn parse_number(s: &str) -> Result<i32, String> {
+    match s.parse::<i32>() {
+        Ok(num) => Ok(num),
+        Err(_) => Err("invalid digit found in string".to_string()),
+    }
 }
 
 // Exercise 3
@@ -35,8 +38,13 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        // Hmm...? Why is this only returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        if value < 0 {
+            Err(CreationError::Negative)
+        } else if value == 0 {
+            Err(CreationError::Zero)
+        } else {
+            Ok(PositiveNonzeroInteger(value as u64))
+        }
     }
 }
 
